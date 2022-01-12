00832ab846ae42a71302fedcdb9011bed72a7768
Patrick van Halm <pvanhalm00@gmail.com>
2022-01-12 11:35:30 +0100

    Add missing comments in utils

--- Assets/Scripts/Utilities/IEnumerableExtension.cs
+++ Assets/Scripts/Utilities/IEnumerableExtension.cs
@@ -3,6 +3,12 @@ using System.Linq;
 
 public static class IEnumerableExtension
 {
+    /// <summary>
+    /// Select a random item using UnityEngine.Random from an IEnumerable
+    /// </summary>
+    /// <typeparam name="T">Any type</typeparam>
+    /// <param name="vs">The IEnumerable</param>
+    /// <returns></returns>
     public static T Random<T>(this IEnumerable<T> vs)
     {
         return vs.ElementAt(UnityEngine.Random.Range(0, vs.Count()));
